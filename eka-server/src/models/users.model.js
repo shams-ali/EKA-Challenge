83884c389937afb4db24584806f5531e0d742c55
Shams Ali <ali.s90@outlook.com>
2017-09-23 19:58:11 -0500

    (initialize) authentication

---
+++ eka-server/src/models/users.model.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+// users-model.js - A KnexJS
+// 
+// See http://knexjs.org/
+// for more of what you can do here.
+module.exports = function (app) {
+  const db = app.get('knexClient');
+
+    db.schema.hasTable('users').then(exists => {
+      if(!exists) {
+        db.schema.createTable('users', table => {
+          table.increments('id');
+        
+          table.string('email').unique();
+          table.string('password');
+        
+        
+        })
+        .then(
+          () => console.log('Updated users table'),
+          e => console.error('Error updating users table', e)
+        );
+      }
+    });
+
+  return db;
+};
--- eka-server/src/services/index.js
+++ eka-server/src/services/index.js
@@ -1,5 +1,7 @@
 const demographics = require('./demographics/demographics.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(demographics);
+  app.configure(users);
 };
