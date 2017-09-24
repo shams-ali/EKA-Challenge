7cee0c82f421f1e679876dc8bee3d940126c5d20
Shams Ali <ali.s90@outlook.com>
2017-09-23 20:15:55 -0500

    (update) demographics to include user from users service

--- eka-server/src/services/demographics/demographics.hooks.js
+++ eka-server/src/services/demographics/demographics.hooks.js
@@ -1,18 +1,31 @@
 const { authenticate } = require('feathers-authentication').hooks;
+const { populate } = require('feathers-hooks-common');
+const processDemographics = require('../../hooks/process-demographics');
 
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [ processDemographics() ],
+    update: [ processDemographics() ],
+    patch: [ processDemographics() ],
     remove: []
   },
 
   after: {
-    all: [],
+    all: [
+      populate({
+        schema: {
+          include: [{
+            service: 'users',
+            nameAs: 'user',
+            parentField: 'userId',
+            childField: '_id'
+          }]
+        }
+      })
+    ],
     find: [],
     get: [],
     create: [],
