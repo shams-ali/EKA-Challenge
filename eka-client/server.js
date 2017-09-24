ac7c8ef7679cf4feab57fd6cd42401a5b7073238
Shams Ali <ali.s90@outlook.com>
2017-09-23 19:39:16 -0500

    (rename) client folder

---
+++ eka-client/server.js
@@ -0,0 +1,19 @@
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const proxy = require('express-http-proxy');
+
+const app = express();
+const PORT = process.env.PORT;
+const API_SERVER_URL = process.env.API_SERVER_URL;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api', proxy(API_SERVER_URL));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
+
+app.listen(PORT, (err) => console.warn(err || `listening on ${ PORT }`));
+
